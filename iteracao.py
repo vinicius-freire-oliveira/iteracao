f39ee9cbe9ab9b936e9df2430feaba79414f710f
Vinícius-Freire <120794204+vinicius-freire-oliveira@users.noreply.github.com>
2024-06-11 10:00:55 -0300

    Add files via upload
    Define uma variável 'palavra' com o valor "Data Science, Itera sobre cada caractere na string 'Data  Science' e verifica se o caractere atual é igual a "c"

---
+++ iteracao.py
@@ -0,0 +1,12 @@
+# Define uma variável 'palavra' com o valor "Data Science"
+palavra = "Data Science"
+
+# Itera sobre cada caractere na string 'palavra'
+for letra in palavra:
+    # Verifica se o caractere atual é igual a "c"
+    if(letra == "c"):
+        # Se a condição for verdadeira, imprime "Achou!"
+        print("Achou!")
+    else:
+        # Se a condição for falsa, imprime "Não achou"
+        print("Não achou")
